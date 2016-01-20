1f9a3c1f83eea1e81a153aaefdf1e9b539f5f41d
Dixit Patel <talkwithdixit@gmail.com>
2016-01-19 23:09:30 -0500

    Added Prototype to point the method for all the new object creation

--- task.js
+++ task.js
@@ -108,23 +108,25 @@
         console.log('Saving Task ' + this.name);
     };
 
+    // Prototype is An encapsulation of properties that an object links to
+
     var taskOne = new Task('Create a demo for Constructors'),
         taskTwo = new Task('Create a demo for Modules'),
-        taskThree = new Task('Create a demo for Singltons'),
-        taskFour = new Task('Create a demo for Prototypes');
+        taskThr = new Task('Create a demo for Singltons'),
+        taskFor = new Task('Create a demo for Prototypes');
     console.log(taskOne);
-    console.log(taskOne.__proto__)
+    console.log(taskOne.__proto__);
     console.log(taskTwo);
-    console.log(taskTwo.__proto__)
-    console.log(taskThree);
-    console.log(taskThree.__proto__)
-    console.log(taskFour);
-    console.log(taskFour.__proto__)
-    console.log(taskTwo.__proto__ === taskThree.__proto__);
+    console.log(taskTwo.__proto__);
+    console.log(taskThr);
+    console.log(taskThr.__proto__);
+    console.log(taskFor);
+    console.log(taskFor.__proto__);
+    console.log(taskTwo.__proto__ === taskThr.__proto__);
 
     taskOne.complete();
     taskTwo.save();
-    taskThree.save();
-    taskFour.save();
+    taskThr.save();
+    taskFor.save();
 
 })();
